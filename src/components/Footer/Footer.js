82aadc5ea8323b5587f1cbdcb069247965f212c2
Alejandro <aleeeehs@icloud.com>
2018-10-09 19:48:13 +1300

    Login integration and Search bar

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,13 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-import './Footer.css';
-
+import "./Footer.css";
+//llalalala
 class Footer extends Component {
   render() {
     return (
       <div className="row" id="footer">
         <div className="medium-12 columns">
-          <p>(C) Srinivas Tamada Production <a href="https://www.9lessons.info">9lessons.info</a></p>
+          <p>TreeCo</p>
         </div>
       </div>
     );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,27 +1,23 @@
-import React, {Component} from 'react';
-import {Redirect} from 'react-router-dom';
-import './Home.css';
-import {PostData} from '../../services/PostData';
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import "./Home.css";
+import { PostData } from "../../services/PostData";
 import UserFeed from "../UserFeed/UserFeed";
-import ReactConfirmAlert, { confirmAlert } from 'react-confirm-alert'; 
-import '../../styles/react-confirm-alert.css';
+import ReactConfirmAlert, { confirmAlert } from "react-confirm-alert";
+import "../../styles/react-confirm-alert.css";
 
 class Home extends Component {
- 
-
   constructor(props) {
     super(props);
 
     this.state = {
-      data:[],
-      userFeed: '',
+      data: [],
+      userFeed: "",
       redirectToReferrer: false,
-      name:'',
+      name: ""
     };
 
-    this.getUserFeed = this
-      .getUserFeed
-      .bind(this);
+    this.getUserFeed = this.getUserFeed.bind(this);
     this.feedUpdate = this.feedUpdate.bind(this);
     this.onChange = this.onChange.bind(this);
     this.deleteFeed = this.deleteFeed.bind(this);
@@ -31,28 +27,27 @@ class Home extends Component {
   }
 
   componentWillMount() {
-
-   if(sessionStorage.getItem("userData")){
-    this.getUserFeed();
-   }
-  
-   else{
-    this.setState({redirectToReferrer: true});
-   }
-   
-
+    if (sessionStorage.getItem("userData")) {
+      this.getUserFeed();
+    } else {
+      this.setState({ redirectToReferrer: true });
+    }
   }
 
   feedUpdate(e) {
     e.preventDefault();
     let data = JSON.parse(sessionStorage.getItem("userData"));
-    let postData = { user_id: data.userData.user_id, token: data.userData.token, feed: this.state.userFeed };
+    let postData = {
+      user_id: data.userData.user_id,
+      token: data.userData.token,
+      feed: this.state.userFeed
+    };
     if (this.state.userFeed) {
-      PostData('feedUpdate', postData).then((result) => {
+      PostData("feedUpdate", postData).then(result => {
         let responseJson = result;
         let K = [responseJson.feedData].concat(this.state.data);
         console.log(K);
-       this.setState({data: K , userFeed:''});
+        this.setState({ data: K, userFeed: "" });
       });
     }
   }
@@ -62,91 +57,99 @@ class Home extends Component {
     return date;
   }
 
-  deleteFeedAction(e){
+  deleteFeedAction(e) {
     console.log("HI");
-  let updateIndex=e.target.getAttribute('value');
-  let feed_id=e.target.getAttribute('data');
-  
-  let data = JSON.parse(sessionStorage.getItem("userData"));
+    let updateIndex = e.target.getAttribute("value");
+    let feed_id = e.target.getAttribute("data");
 
-  let postData = { user_id: data.userData.user_id, token: data.userData.token, feed_id: feed_id };
-  if (postData) {
-    PostData('feedDelete', postData).then((result) => {
-     this
-     .state
-     .data.splice(updateIndex,1);
-    this.setState({data:this
-      .state
-      .data});
+    let data = JSON.parse(sessionStorage.getItem("userData"));
+
+    let postData = {
+      user_id: data.userData.user_id,
+      token: data.userData.token,
+      feed_id: feed_id
+    };
+    if (postData) {
+      PostData("feedDelete", postData).then(result => {
+        this.state.data.splice(updateIndex, 1);
+        this.setState({
+          data: this.state.data
+        });
+      });
+    }
+  }
+
+  deleteFeed(e) {
+    confirmAlert({
+      title: "",
+      message: "Are you sure?",
+      childrenElement: () => "",
+      confirmLabel: "Delete",
+      cancelLabel: "Cancel",
+      onConfirm: () => this.deleteFeedAction(e),
+      onCancel: () => ""
     });
-  }
-  }
-
-  deleteFeed(e){
-
-
-      confirmAlert({
-        title: '',                        
-        message: 'Are you sure?',               
-        childrenElement: () => '',       
-        confirmLabel: 'Delete',                          
-        cancelLabel: 'Cancel',                            
-        onConfirm: () => this.deleteFeedAction(e),    
-        onCancel: () => '',      
-      })
- 
-    
-
-       
   }
 
   getUserFeed() {
-  
     let data = JSON.parse(sessionStorage.getItem("userData"));
-    this.setState({name:data.userData.name});
-    let postData = { user_id: data.userData.user_id, token: data.userData.token}; 
+    this.setState({ name: data.userData.name });
+    let postData = {
+      user_id: data.userData.user_id,
+      token: data.userData.token
+    };
 
     if (data) {
-      PostData('feed', postData).then((result) => {
+      PostData("feed", postData).then(result => {
         let responseJson = result;
-        this.setState({data: responseJson.feedData});
+        this.setState({ data: responseJson.feedData });
         console.log(this.state);
       });
     }
-    
   }
 
-  onChange(e){
-    this.setState({userFeed:e.target.value});
-   }
-   logout(){
-     sessionStorage.setItem("userData",'');
-     sessionStorage.clear();
-     this.setState({redirectToReferrer: true});
-   }
+  onChange(e) {
+    this.setState({ userFeed: e.target.value });
+  }
+  logout() {
+    sessionStorage.setItem("userData", "");
+    sessionStorage.clear();
+    this.setState({ redirectToReferrer: true });
+  }
 
   render() {
     if (this.state.redirectToReferrer) {
-      return (<Redirect to={'/login'}/>)
+      return <Redirect to={"/login"} />;
     }
 
     return (
       <div className="row" id="Body">
         <div className="medium-12 columns">
-        <a href="#" onClick={this.logout} className="logout">Logout</a>
-        <form onSubmit={this.feedUpdate} method="post">
-            <input name="userFeed" onChange={this.onChange} value={this.state.userFeed} type="text" placeholder="What's up?"/>
+          <a href="#" onClick={this.logout} className="logout">
+            Logout
+          </a>
+          <form onSubmit={this.feedUpdate} method="post">
+            <input
+              name="userFeed"
+              onChange={this.onChange}
+              value={this.state.userFeed}
+              type="text"
+              placeholder="Start looking for the perfect tree here"
+            />
             <input
               type="submit"
-              value="Post"
+              value="Search"
               className="button"
-              onClick={this.feedUpdate}/>
-              </form>
-        
+              onClick={this.feedUpdate}
+            />
+          </form>
         </div>
-        <UserFeed feedData = {this.state.data}  deleteFeed = {this.deleteFeed} convertTime={this.convertTime} name={this.state.name}/>
-        
-      
+        <UserFeed
+          feedData={this.state.data}
+          deleteFeed={this.deleteFeed}
+          convertTime={this.convertTime}
+          name={this.state.name}
+        />
       </div>
     );
   }
