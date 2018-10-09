fb02cbd7373ab16469245227282dde955b23579f
Alejandro <aleeeehs@icloud.com>
2018-10-09 19:46:13 +1300

    Login integration and Search bar

--- src/App.js
+++ src/App.js
@@ -1,40 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './styles/foundation.min.css';
-import './styles/custom.css';
-import Routes from './routes';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import "./styles/foundation.min.css";
+import "./styles/custom.css";
+import Routes from "./routes";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
-import MobileHeader from './components/MobileHeader/MobileHeader';
-
+import MobileHeader from "./components/MobileHeader/MobileHeader";
 
 class App extends Component {
-
-  constructor(){
+  constructor() {
     super();
-    this.state={
-      appName: "Banana",
+    this.state = {
+      appName: "Plant a Tree",
       home: false
-    }
+    };
   }
 
   render() {
     return (
       <div className="off-canvas-wrapper">
-      <div className="off-canvas-wrapper-inner" data-off-canvas-wrapper>
-
-     
-
-        <div className="off-canvas-content" data-off-canvas-content>
-          <MobileHeader name={this.state.appName}/>
-          <Header name={this.state.appName}/>
-          <Routes name={this.state.appName}/>
-          <hr/>
-         <Footer/>
+        <div className="off-canvas-wrapper-inner" data-off-canvas-wrapper>
+          <div className="off-canvas-content" data-off-canvas-content>
+            <MobileHeader name={this.state.appName} />
+            <Header name={this.state.appName} />
+            <Routes name={this.state.appName} />
+            <hr />
+            <Footer />
+          </div>
         </div>
       </div>
-    </div>
     );
   }
 }
